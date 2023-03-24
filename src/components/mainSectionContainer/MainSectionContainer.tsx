18524816fabf6465054e8ca4635c1cae581a89e8
yno1109 <yno1109@naver.com>
2023-03-24 11:03:59 +0900

    [add] TodaysRecommendMenuBanner component func complete

--- src/components/mainSectionContainer/MainSectionContainer.tsx
+++ src/components/mainSectionContainer/MainSectionContainer.tsx
@@ -1,10 +1,12 @@
 import CategoryContainer from "./categoryContainer/CategoryContainer";
 import "./css/MainSectionContainer.css";
+import TodaysRecommendMenuBanner from "./todaysRecommendMenuBanner/TodaysRecommendMenuBanner";
 
 const MainSectionContainer = () => {
   return (
     <div className="mainSectionContainer">
       <CategoryContainer />
+      <TodaysRecommendMenuBanner />
     </div>
   );
 };
